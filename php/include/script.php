0694526e0213f12b76a94f5e854ec7b0c8dcbaa0
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-10-27 15:23:09 +0900

    22.10.27

---
+++ php/include/script.php
@@ -0,0 +1,9 @@
+<!-- script -->
+<script src="../assets/js/headermenu.js"></script>
+<script src="../assets/js/loginpopup.js"></script>
+<script src="../assets/js/notice.js"></script>
+<script src="../assets/js/swiper.js"></script>
+<script src="../assets/js/slider.js"></script>
+<script src="../assets/js/agree.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- //script -->
