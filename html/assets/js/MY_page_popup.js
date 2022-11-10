269507f02365dd1aeca822caa2f2193ad69f8caa
이유나 <109262091+fkdldhs8484@users.noreply.github.com>
2022-11-11 04:46:11 +0900

    ddd

--- html/assets/js/MY_page_popup.js
+++ html/assets/js/MY_page_popup.js
@@ -1,5 +1,5 @@
 const Mppopup = document.querySelector(".MP__popup");
-const MppopupBnt = document.querySelector(".MP__page__ch__btn li a");
+const MppopupBnt = document.querySelector(".MP__page__ch__btn a");
 const MppopupMain = document.querySelector(".MP__Popup__main");
 
 MppopupBnt.addEventListener("click", () => {
